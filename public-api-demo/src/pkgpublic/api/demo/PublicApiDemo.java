ee6ec7a9dbaca63d5b8035ee4813dacb7508c407
Logan <logan.ash2025@gmail.com>
2024-02-01 17:51:55 -0500

    setting up dev environment

---
+++ public-api-demo/src/pkgpublic/api/demo/PublicApiDemo.java
@@ -0,0 +1,12 @@
+package pkgpublic.api.demo;
+
+public class PublicApiDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+    
+}
